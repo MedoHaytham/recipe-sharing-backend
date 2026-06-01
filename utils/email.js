9c14828dffbf14cef721196d93ac8daf86998718
MedoHaytham <104214885+MedoHaytham@users.noreply.github.com>
2026-06-01 19:35:04 +0300

    feat: add nodemailer utility function for sending emails

--- utils/email.js
+++ utils/email.js
@@ -1,34 +1,29 @@
 const nodemailer = require('nodemailer');
 
 const sendEmail = async options => {
-  // 1) create transporter with connection & socket timeouts
+  // 1) create transporter
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    connectionTimeout: 10000,   // 10s to establish connection
-    greetingTimeout:  10000,    // 10s for server greeting
-    socketTimeout:    15000,    // 15s for socket inactivity
+    port: Number(process.env.EMAIL_PORT),
+    secure: false,
     auth: {
       user: process.env.EMAIL_USERNAME,
       pass: process.env.EMAIL_PASSWORD
-    }
+    },
+    connectionTimeout: 10000,
+    greetingTimeout: 10000,
+    socketTimeout: 10000
   });
-
   // 2) Define email options
   const mailOptions = {
     from: 'Mohamed Haytham <medo@gmail.com>',
     to: options.email,
     subject: options.subject,
     text: options.message
-  };
-
-  // 3) Send with a hard 20-second timeout
-  await Promise.race([
-    transporter.sendMail(mailOptions),
-    new Promise((_, reject) =>
-      setTimeout(() => reject(new Error('Email sending timed out after 20 seconds')), 20000)
-    )
-  ]);
-};
+  }
+  // 3) send the email
+  await transporter.sendMail(mailOptions);
+}
 
 module.exports = sendEmail;
+
