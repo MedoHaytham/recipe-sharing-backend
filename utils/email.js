49251b64a16770e8b8062ba88db04223851f1bc3
MedoHaytham <104214885+MedoHaytham@users.noreply.github.com>
2026-06-01 20:01:45 +0300

    feat: implement user authentication controllers and email service for password management

--- utils/email.js
+++ utils/email.js
@@ -1,38 +1,25 @@
 const nodemailer = require('nodemailer');
 
 const sendEmail = async options => {
-  console.log('EMAIL_HOST', process.env.EMAIL_HOST);
-  console.log('EMAIL_PORT', process.env.EMAIL_PORT);
-  console.log('EMAIL_USERNAME', process.env.EMAIL_USERNAME);
-
-  console.log('Before transporter');
-
+  // 1) create transporter
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
-    port: Number(process.env.EMAIL_PORT),
-    secure: false,
-    family: 4,
+    port: process.env.EMAIL_PORT,
     auth: {
       user: process.env.EMAIL_USERNAME,
       pass: process.env.EMAIL_PASSWORD
-    },
-    connectionTimeout: 10000,
-    greetingTimeout: 10000,
-    socketTimeout: 10000
+    }
   });
-
+  // 2) Define email options
   const mailOptions = {
     from: 'Mohamed Haytham <medo@gmail.com>',
     to: options.email,
     subject: options.subject,
     text: options.message
-  };
-
-  console.log('Before sendMail');
-
-  const info = await transporter.sendMail(mailOptions);
-
-  console.log('Email sent:', info.messageId);
-};
+  }
+  // 3) send the email
+  await transporter.sendMail(mailOptions);
+}
 
 module.exports = sendEmail;
+
